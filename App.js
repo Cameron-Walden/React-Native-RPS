585d6405e916baa15ebe745da861d68099300c37
Cameron <cameronkwalden@gmail.com>
2021-12-15 15:55:34 -0800

    setting up the homescreen

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+const cog = require('./assets/cog.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text style={styles.container}>🪨 🧻 </Text> */}
+      {/* <Image source={require(cog)} /> */}
+      <Text style={{fontSize: 65}}> 🪨 🧻 ✂️ </Text>
+      <View style={styles.button}>
+        <Button title="Press the Dang Button!" />
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 30,
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 30,
+    width: 250,
+  }
 });
