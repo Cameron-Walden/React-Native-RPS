b4bfe23268f056a92b4d729a41ed4028f9a74dac
Cameron <cameronkwalden@gmail.com>
2021-12-16 08:34:19 -0800

    cleaning up/imports components

--- App.js
+++ App.js
@@ -1,32 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-const cog = require('./assets/cog.png')
+import React, { useState } from 'react';
+import {Text, View, StyleSheet } from 'react-native';
+import Rounds from './src/Rounds.js'
+import Characters from './src/Characters.js';
+import CharcaterStyling from './src/CharacterStyling.js';
+import GameButton from './src/GameButton.js';
+import cog from './assets/cog.png';
 
 export default function App() {
-  return (
+  const [gamePrompt, setGamePrompt] = useState('Pick a character!');
+  const [playerChoice, setPlayerChoice] = useState({});
+  const [computerChoice, setComputerChoice] = useState({});
+  // const [playing, setPLaying] = useState(false);
+
+  const onPress = (playerChoice) => {
+    const [result, compChoice] = Rounds(playerChoice);
+    
+    const newPlayerChoice = Characters.find(character => character.name === playerChoice);
+    const newComputerChoice = Characters.find(character => character.name === compChoice);
+
+    setGamePrompt(result);
+    setPlayerChoice(newPlayerChoice);
+    setComputerChoice(newComputerChoice);
+  };
+
+  return ( 
     <View style={styles.container}>
-      {/* <Text style={styles.container}>🪨 🧻 </Text> */}
-      {/* <Image source={require(cog)} /> */}
-      <Text style={{fontSize: 65}}> 🪨 🧻 ✂️ </Text>
-      <View style={styles.button}>
-        <Button title="Press the Dang Button!" />
+      <View style={styles.characterContainer}>
+        <CharcaterStyling player="Player" choice={playerChoice}/>
+        <Text>VS</Text>
+        <CharcaterStyling player="Computer" choice={computerChoice}/>
       </View>
+      {Characters.map(character => {
+        return (
+          <GameButton
+            key={character.name}
+            name={character.name}
+            onPress={onPress}
+          />
+        )
+      })}
     </View>
   );
-}
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'tan'
   },
-  header: {
-    fontSize: 30,
-    marginBottom: 10,
+  characterContainer: { 
+    margin: 10,
+    borderWidth: 2,
+    paddingTop: 100,
+    shadowRadius: 50,
+    paddingBottom: 100,
+    borderColor: 'black',
+    shadowOpacity: 0.90,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'space-around',
+    // shadowColor: 'black',
+    shadowOffset: { height: 5, width: 5 },
   },
-  button: {
-    marginTop: 30,
-    width: 250,
-  }
 });
