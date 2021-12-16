ed75367eaca4575ae9047422f1b8940eb023aea2
Cameron <cameronkwalden@gmail.com>
2021-12-15 19:27:03 -0800

    brings in playable charcters. still working on filepaths

---
+++ src/Characters.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Image } from 'react-native';
+const rock = require('../assets/playables/rock.png')
+const paper = require('../assets/playables/paper.png')
+const scissors = require('../assets/playables/scissors.png')
+
+// const characters = [rock, paper, scissors]
+
+const Characters = [
+  {
+    name: 'rock',
+    characterImagePath: '',
+  },
+  {
+    name: 'paper',
+    characterImagePath: '',
+  },
+  {
+    name: 'scissors',
+    characterImagePath: '',
+  },
+];
+
+export default Characters;
